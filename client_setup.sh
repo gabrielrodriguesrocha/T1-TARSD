fdbd59f4534e9a8a3064e47dba435557fa8403fd
gabrielrodriguesrocha <gabrielrodrigues_rocha@hotmail.com>
2019-06-04 22:30:10 -0300

    Changed client-side requirements

--- client_setup.sh
+++ client_setup.sh
@@ -6,5 +6,4 @@ cd T1-TARSD/docker_client
 chmod +x /vagrant/token/join.sh
 bash /vagrant/token/join.sh
 sudo docker build -t app_client .
-sudo docker service create --name app_client_service --network ClusterNet --replicas 3 -p 5001:80 app_client
-sudo python3 info_server.py
+sudo python3 info_server.py &
